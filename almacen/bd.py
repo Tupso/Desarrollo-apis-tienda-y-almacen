39238ef642c609e2fa72e7d5c740feda5c647415
Tupso <74798365+Tupso@users.noreply.github.com>
2024-02-14 12:35:47 +0100

    Revert "."
    This reverts commit c4695737c00fcfa36cd463e3aea93b831f12c941.

---
+++ almacen/bd.py
@@ -0,0 +1,33 @@
+import sqlite3
+import os
+
+
+def create_db(db_path):
+    if not os.path.exists(db_path):
+        os.makedirs(os.path.dirname(db_path), exist_ok=True)
+
+        # Conexión a la base de datos
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        # Creación de la tabla de productos si no existe
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS articulos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                descripcion TEXT,
+                cantidad INTEGER NOT NULL DEFAULT 0,
+                disponible BOOLEAN NOT NULL DEFAULT TRUE
+            )
+        ''')
+        conn.commit()
+        # conn.close()
+    else:
+        print(f"La base de datos ya existe en: {db_path}")
+
+# Obtener los resultados
+# cursor.execute("SELECT * FROM articulos")
+# results = cursor.fetchall()
+# print(results)
+
+
